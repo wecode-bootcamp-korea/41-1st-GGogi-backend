c13124d8b376348e3864df43fdc6d96317b89485
kss7082 <118211014+kss7082@users.noreply.github.com>
2023-01-11 14:05:23 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRouter = require("./userRouter");
 const cartRouter = require("./cartRouter");
+const productRouter = require("./productRoter");
 
 router.use("/users", userRouter.router);
 router.use("/carts", cartRouter.router);
+router.use("/products", productRouter.router);
 
 module.exports = router;
